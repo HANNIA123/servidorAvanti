1f27d9ee44adedec767c74f4a96effd237c51a7f
Carolina Serrano <69402196+Caro2518@users.noreply.github.com>
2024-04-05 00:34:21 -0600

    Update solicitudRoutes.js
    Agregue la ruta para obtener la solicitud de acuerdo al id del horario.

--- routes/solicitudRoutes.js
+++ routes/solicitudRoutes.js
@@ -45,6 +45,43 @@ solicitudRouter.get('/api/obtenersolicitud/:id', async (req, res) => {
 });
 
 
+//Ruta para obtener la solicitud de acuerdo al id del horario
+solicitudRouter.get('/obtenersolicitudhorario/:id', async (req, res) => {
+    const horarioId = req.params.id;
+    console.log("horarioId", horarioId)
+    try {
+        // Consultar la solicitud con el horario_id proporcionado
+        const solicitudQuery = query(collection(db, 'solicitud'), where('horario_id', '==', horarioId));
+        const solicitudSnapshot = await getDocs(solicitudQuery);
+
+        // Verificar si se encontró alguna solicitud
+        if (!solicitudSnapshot.empty) {
+
+            const solicitudData = solicitudSnapshot.docs[0].data();
+        
+            // Enviar el horario_id como respuesta en formato JSON
+            res.json({ 
+                conductor_id: solicitudData.conductor_id || '',
+                horario_id: solicitudData.horario_id || '',
+                horario_trayecto: solicitudData.horario_trayecto || '',
+                parada_id: solicitudData.parada_id || '',
+                pasajero_id: solicitudData.pasajero_id || '',
+                solicitud_date: solicitudData.solicitud_date || '',
+                solicitud_status: solicitudData.solicitud_status || '',
+                viaje_id: solicitudData.viaje_id || '',
+                solicitud_activa_con: solicitudData.solicitud_activa_con || '',
+                solicitud_activa_pas: solicitudData.solicitud_activa_pas || '', 
+        });
+        } else {
+            res.status(404).json({ error: 'La solicitud no existe' });
+        }
+    } catch (error) {
+        console.error('Error al obtener solicitud desde Firestore:', error);
+        res.status(500).json({ error: 'Error al obtener solicitud desde Firestore' });
+    }
+});
+
+
 
 
 // Ruta para registrar un viaje
