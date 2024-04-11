beecc3e1a8ddcbcbeb70094b8c765774a1278214
HANNIA123 <hannia23413@gmail.com>
2024-04-10 23:21:21 -0600

    Solicitudes

--- routes/solicitudRoutes.js
+++ routes/solicitudRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { doc, getDoc, addDoc, collection, query, where, getDocs} = require('firebase/firestore');
 const db = require('../firebase');
+const {updateDoc} = require("@firebase/firestore");
 
 const solicitudRouter = express.Router();
 
@@ -97,6 +98,69 @@ console.log("Enviadaa")
         res.status(500).json({ success: false, message: 'Error al agregar solicitud a Firestore' });
     }
 });
+
+//Buscar las solcitudes de un conductor
+solicitudRouter.get('/obtenersolicitudesconductor/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        // Assuming 'viajes' is your collection name
+        const solicitudRef = collection(db, 'solicitud');
+        const solicitudQuery = query(solicitudRef, where('conductor_id', '==', userId));
+        const solicitudSnapshot = await getDocs(solicitudQuery);
+
+        if (solicitudSnapshot.docs.length > 0) {
+            // Map the documents to an array of JSON objects
+            const solicitudData = solicitudSnapshot.docs.map(doc => {
+                const data = doc.data();
+                return {
+                    solicitud_id:     doc.id, // Agregar el Id
+                    conductor_id: data.conductor_id || '',
+                    horario_id: data.horario_id || '',
+                    horario_trayecto: data.horario_trayecto || '',
+                    parada_id: data.parada_id || '',
+                    pasajero_id:data.pasajero_id || '',
+                    solicitud_date:data.solicitud_date || '',
+                    solicitud_status:data.solicitud_status || '',
+                    viaje_id:data.viaje_id || '',
+                    solicitud_activa_con:data.solicitud_activa_con || '',
+                    solicitud_activa_pas:data.solicitud_activa_pas || '',
+                };
+            });
+
+            // Send the array of JSON objects as a response
+            res.json(solicitudData);
+        } else {
+            res.status(404).json({ error: 'No se encontraron solicitudes para el conductor' });
+        }
+    } catch (error) {
+        console.error('Error al obtener documentos desde Firestore:', error);
+        res.status(500).json({ error: 'Error al obtener documentos desde Firestore' });
+    }
+});
+
+solicitudRouter.put('modificarstatussolicitud/:id/:status', async (req, res) => {
+    const paradaId = req.params.id;
+    const nuevoStatus = req.params.status;
+
+    try {
+        const paradaRef = doc(db, 'solicitud', paradaId);
+        const paradaDoc = await getDoc(paradaRef);
+
+        if (paradaDoc.exists()) {
+            // Modificar solo el campo status de la parada
+            await updateDoc(paradaRef, { solicitud_status: nuevoStatus });
+            console.log("Solicitud modificada ")
+            res.json({ message: 'Estado de la solicitud modificado correctamente' });
+        } else {
+            res.status(404).json({ error: 'La parada no existe' });
+        }
+    } catch (error) {
+        console.error('Error al modificar el estado de la solicitud en Firestore:', error);
+        res.status(500).json({ error: 'Error al modificar el estado de la parada en Firestore' });
+    }
+});
+
 
 
 module.exports = solicitudRouter;
